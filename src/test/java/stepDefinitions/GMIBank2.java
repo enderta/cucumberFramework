c90ff64b39c3992972dc3760e4c130c006665c66
enderta <endertanver@gmail.com>
2022-08-18 13:40:00 +0100

    gmi

--- src/test/java/stepDefinitions/GMIBank2.java
+++ src/test/java/stepDefinitions/GMIBank2.java
@@ -1,10 +1,26 @@
 package stepDefinitions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.*;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import pages.GMIPages;
 import utilities.BrowserUtils;
 import utilities.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
 
 public class GMIBank2 {
 
@@ -25,6 +41,38 @@ public class GMIBank2 {
     String text = Driver.get().findElement(By.xpath("//span[.='Joe King']")).getText();
     Assert.assertEquals("Joe King", text);
 
+  }
+  String tkn= "";
+  @Given("user is logging to the GMI API")
+  public void user_is_logging_to_the_gmi_api() throws JsonProcessingException {
+    baseURI="https://www.gmibank.com/api/";
+    Map<String,Object> bdy= new HashMap<>();
+    bdy.put("username","team18_admin");
+    bdy.put("password","Team18admin");
+    bdy.put("rememberme","true");
+    ObjectMapper mapper = new ObjectMapper();
+    String s = mapper.writeValueAsString(bdy);
+    Response authorization = given().contentType(ContentType.JSON)
+            .accept(ContentType.JSON).
+                    body(s).
+                    when().
+                    post("authenticate");
+
+
+    tkn = authorization.jsonPath().getString("id_token");
+
+
+   /* List<Object> list = authorization.jsonPath().getList("");*/
+  }
+  @When("user sends get request to the API")
+  public void user_sends_get_request_to_the_api() {
+    Response authorization = given().accept(ContentType.JSON).
+            header("Authorization", "Bearer " + tkn).
+            when().
+            get("tp-customers").prettyPeek();
+    JsonPath jp = authorization.jsonPath();
+    List<Map<String,Object>> list = jp.getList("");
+    list.stream().map(x->x.get("id")).forEach(System.out::println);
   }
 
 }
--- src/test/resources/features/gmibank.feature
+++ src/test/resources/features/gmibank.feature
@@ -1,5 +1,5 @@
 Feature: GMI Bank
-  @gmi
+
   Scenario:  Login
     Given User is on the GMIBank login page
     When User enters valid "gino.wintheiser" and "%B6B*q1!TH"
@@ -10,7 +10,7 @@ Feature: GMI Bank
     When user sends get request to the DB
     Then user should be able to login successfully DB
 
-
+  @gmi
   Scenario: Login API
     Given user is logging to the GMI API
     When user sends get request to the API
