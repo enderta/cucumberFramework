333d9be101e3c537e7b411caf6620b9c2052ba50
enderta <endertanver@gmail.com>
2022-12-26 13:27:43 +0000

    apidbGMI

--- src/test/java/stepDefinitions/CarsOwnerApiDb.java
+++ src/test/java/stepDefinitions/CarsOwnerApiDb.java
@@ -38,15 +38,16 @@ import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeThat;
+
 public class CarsOwnerApiDb {
 @Test
 public void test1() throws SQLException, JsonProcessingException {
 
-	baseURI="http://localhost:3001";
-	Map<String, Object> userBdy= new HashMap<>();
+	baseURI = "http://localhost:3001";
+	Map<String, Object> userBdy = new HashMap<>();
 	userBdy.put("email", "John@gmail.com");
 	userBdy.put("password", "John123");
-	ObjectMapper objectMapper=new ObjectMapper();
+	ObjectMapper objectMapper = new ObjectMapper();
 	String json = objectMapper.writeValueAsString(userBdy);
 	Response register = given().contentType(ContentType.JSON).body(json).when().post("/register");
 	JsonPath jsonPath = register.jsonPath();
@@ -55,9 +56,34 @@ public void test1() throws SQLException, JsonProcessingException {
 	Assert.assertEquals("User created", msgRes);
 	Response login = given().contentType(ContentType.JSON).body(json).when().post("/login");
 	String token = login.jsonPath().getString("token");
+
+
+	Map<String, Object> carBdy = new HashMap<>();
+	carBdy.put("make", "Toyota");
+	carBdy.put("model", "Camry");
+	carBdy.put("vin", 2019);
+	carBdy.put("plate", "123456");
+	carBdy.put("owner_id", 1);
+	String jsonCar = objectMapper.writeValueAsString(carBdy);
+	Response postCar = given().contentType(ContentType.JSON).header("Authorization", token).body(jsonCar).when().post("/cars");
+	postCar.then().statusCode(201);
+
 	Response cars = given().contentType(ContentType.JSON).header("Authorization", token).when().get("/cars");
+
+	cars.then().statusCode(200);
 	List<Map<String, Object>> carsList = cars.jsonPath().getList("");
-	System.out.println(carsList);
+	int lastCarId = (int) carsList.get(carsList.size() - 1).get("id");
+	Response car = given().contentType(ContentType.JSON).header("Authorization", token).when().get("/cars/" + lastCarId);
+	car.then().statusCode(200);
+	given().contentType(ContentType.JSON).header("Authorization", token).when().delete("/cars/" + lastCarId).then().statusCode(200);
+	given().contentType(ContentType.JSON).header("Authorization", token).when().get("/cars/" + lastCarId).then().statusCode(404);
+	Response lastUser = given().contentType(ContentType.JSON).header("Authorization", token).when().get("/users");
+	lastUser.then().statusCode(200);
+	List<Map<String, Object>> usersList = lastUser.jsonPath().getList("");
+	int lastUserId = (int) usersList.get(usersList.size() - 1).get("id");
+	given().contentType(ContentType.JSON).header("Authorization", token).when().delete("/users/" + lastUserId).then().statusCode(200);
+	given().contentType(ContentType.JSON).header("Authorization", token).when().get("/users/" + lastUserId).then().statusCode(404);
+
 
 }
 }
