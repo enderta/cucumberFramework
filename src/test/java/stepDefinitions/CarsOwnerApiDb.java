46b91fce105b2a4717842f1fa6b105306e486ee4
enderta <endertanver@gmail.com>
2022-12-26 14:40:23 +0000

    apidbGMI

--- src/test/java/stepDefinitions/CarsOwnerApiDb.java
+++ src/test/java/stepDefinitions/CarsOwnerApiDb.java
@@ -2,6 +2,7 @@ package stepDefinitions;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.bouncycastle.asn1.cms.MetaData;
 import org.junit.Test;
 import utilities.API;
 
@@ -92,6 +93,45 @@ public void test1() throws SQLException, JsonProcessingException {
 	lastUser.then().statusCode(200);
 	List<Map<String, Object>> usersList = lastUser.jsonPath().getList("");
 	int lastUserId = (int) usersList.get(usersList.size() - 1).get("id");
+	Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cars", "postgres", "ender");
+	Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+	ResultSet resultSet = statement.executeQuery("select * from users where id = " + lastUserId);
+	ResultSetMetaData metaData = resultSet.getMetaData();
+	List<Map<String, Object>> ls = new ArrayList<>();
+	while (resultSet.next()) {
+		Map<String, Object> map = new HashMap<>();
+		for (int i = 1; i <= metaData.getColumnCount(); i++) {
+			map.put(metaData.getColumnName(i), resultSet.getObject(i));
+		}
+		ls.add(map);
+	}
+
+	ResultSet resultSet1 = statement.executeQuery("select * from owners where id = " + lastOwnerId);
+	ResultSetMetaData metaData1 = resultSet1.getMetaData();
+	List<Map<String, Object>> ls1 = new ArrayList<>();
+	while (resultSet1.next()) {
+		Map<String, Object> map = new HashMap<>();
+		for (int i = 1; i <= metaData1.getColumnCount(); i++) {
+			map.put(metaData1.getColumnName(i), resultSet1.getObject(i));
+		}
+		ls1.add(map);
+	}
+
+	ResultSet resultSet2 = statement.executeQuery("select * from cars where id = " + lastCarId);
+	ResultSetMetaData metaData2 = resultSet2.getMetaData();
+	List<Map<String, Object>> ls2 = new ArrayList<>();
+	while (resultSet2.next()) {
+		Map<String, Object> map = new HashMap<>();
+		for (int i = 1; i <= metaData2.getColumnCount(); i++) {
+			map.put(metaData2.getColumnName(i), resultSet2.getObject(i));
+		}
+		ls2.add(map);
+	}
+	Assert.assertEquals(lastCarId, ls2.get(0).get("id"));
+	Assert.assertEquals(lastOwnerId, ls1.get(0).get("id"));
+	Assert.assertEquals(lastUserId, ls.get(0).get("id"));
+
+
 	given().contentType(ContentType.JSON).header("Authorization", token).when().delete("/cars/" + lastCarId).then().statusCode(200);
 	given().contentType(ContentType.JSON).header("Authorization", token).when().delete("/users/" + lastUserId).then().statusCode(200);
 	given().contentType(ContentType.JSON).header("Authorization", token).when().delete("/owners/" + lastOwnerId).then().statusCode(200);
