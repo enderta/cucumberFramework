24b44c6f83b6a9688d6ebd4e1aaac1db13c88a1b
enderta <endertanver@gmail.com>
2023-01-10 15:37:25 +0000

    listsTest

--- src/test/java/stepDefinitions/Study.java
+++ src/test/java/stepDefinitions/Study.java
@@ -2,10 +2,12 @@ package stepDefinitions;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.json.Json;
 
@@ -66,17 +68,60 @@ public void DbTest() throws SQLException {
 }
 
 @Test
-public void testD() throws SQLException {
+public void testD() throws SQLException, JsonProcessingException {
+	Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/emp", "postgres", "ender");
+	Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 	baseURI = "http://localhost:3001/lists";
+	Map<String,Object> bdy= new HashMap<>();
+	bdy.put("name","test");
+	ObjectMapper objectMapper = new ObjectMapper();
+	String json = objectMapper.writeValueAsString(bdy);
+	Response postName = given().accept(ContentType.JSON).when().body(json).post("/name").then().statusCode(201).extract().response();
+	JsonPath post = postName.jsonPath();
+	String nameApi= post.getString("name");
+	Integer idApi = post.getInt("id");
+	ResultSet rs = st.executeQuery("select * from groups");
+	ResultSetMetaData metaData = rs.getMetaData();
+	List<Map<String,Object>> list = new ArrayList<>();
+	while (rs.next()){
+		Map<String,Object> map = new HashMap<>();
+		for (int i = 1; i <= metaData.getColumnCount(); i++) {
+			map.put(metaData.getColumnName(i),rs.getObject(i));
+		}
+		list.add(map);
+	}
+
+	Assert.assertTrue(list.stream().anyMatch(x->x.get("name").equals(nameApi)));
+	Map<String,Object> bdy2=new HashMap<>();
+	bdy2.put("group_id",idApi);
+	bdy2.put("email", Faker.instance().internet().emailAddress());
+	json = objectMapper.writeValueAsString(bdy2);
+	Response postMem = given().contentType(ContentType.JSON).when().body(json).post("/member").then().statusCode(201).extract().response();
+	JsonPath post2 = postMem.jsonPath();
+	String emailApi = post2.getString("email");
+
+	ResultSet rs2 = st.executeQuery("select * from members");
+	metaData = rs.getMetaData();
+	List<Map<String,Object>> list2 = new ArrayList<>();
+	while (rs2.next()){
+		Map<String,Object> map = new HashMap<>();
+		for (int i = 1; i <= metaData.getColumnCount(); i++) {
+			map.put(metaData.getColumnName(i),rs2.getObject(i));
+		}
+		list2.add(map);
+	}
+	Assert.assertTrue(list2.stream().map(x->x.get("members")).collect(Collectors.toList()).stream().anyMatch(x->x.equals(emailApi)));
+
+
+
 
 
 	Response response = given().contentType("application/json").when().get().then().statusCode(200).extract().response();
 	JsonPath jp = response.jsonPath();
 	List<Map<String, Object>> listApi = jp.getList("");
-	Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/emp", "postgres", "ender");
-	Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-	ResultSet rs = st.executeQuery("select * from groups");
-	ResultSetMetaData metaData = rs.getMetaData();
+
+	 rs = st.executeQuery("select * from groups");
+	 metaData = rs.getMetaData();
 	List<Map<String, Object>> listDB = new ArrayList<>();
 	while (rs.next()) {
 		Map<String, Object> map = new HashMap<>();
@@ -90,6 +135,7 @@ public void testD() throws SQLException {
 	JsonPath jp1 = all.jsonPath();
 	List<Map<String, Object>> listApiAll = jp1.getList("");
 	assert listApiAll.size() == listDB.size();
+
 
 
 
